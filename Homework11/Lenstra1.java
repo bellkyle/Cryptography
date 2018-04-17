eae181000943f4aba229285e1002f6a95efe605c
Kyle Bell <bellkyle97@gmail.com>
2018-04-16 23:44:44 -0500

    Update Lenstra1.java

--- Homework11/Lenstra1.java
+++ Homework11/Lenstra1.java
@@ -2,91 +2,101 @@ import java.util.Scanner;
 import java.util.*;
 import java.math.BigInteger;
 
-public class Lenstra {
+public class Lenstra1 {
 	static BigInteger addX;
 	static BigInteger addY;
 	static BigInteger multX;
 	static BigInteger multY;
 	static BigInteger p;
+	static BigInteger d;
 	public static void main(String[] args) {
 		BigInteger A;
 		BigInteger B;
-		BigInteger n;
-		p = BigInteger.valueOf(187);
+		p = BigInteger.valueOf(0);
 		Scanner input = new Scanner(System.in);
 		System.out.println("E:Y^2 = X^3 + AX + B");
 		System.out.println("Enter the A value of the elliptic curve equation");
 		A = input.nextBigInteger();
 		System.out.println("Enter the B value of the elliptic curve equation");
 		B = input.nextBigInteger();
-		System.out.println("Enter the N");
+		System.out.println("Enter the prime value");
 		p = input.nextBigInteger();
-		BigInteger x1;
-		BigInteger y1;
+		BigInteger x;
+		BigInteger y;
+		//BigInteger n;
+		//BigInteger y2;
 		System.out.println("Enter the x value of the first point");
-		x1 = input.nextBigInteger();
+		x = input.nextBigInteger();
 		System.out.println("Enter the y value of the first point");
-		y1 = input.nextBigInteger();
-		
+		y = input.nextBigInteger();
+		multX = x;
+		multY = y;
+		addX = x;
+		addY = y;
 		input.close();
-		//multiplication(x1,y1,n,A);
-		//System.out.println(multX + " " + multY);
+		BigInteger j = BigInteger.valueOf(2);
+		for(int i = 0; i < 1; i++) {
+			if(!addition(x,y,addX,addY,A))
+				System.out.println(d);
+		}
+		System.out.println(addX + " " + addY);
+		while(addition(x,y,addX,addY,A)) {
+			//System.out.println(multX + " " + multY + " " + j + " " + d);
+
+			j = j.add(BigInteger.ONE);
+		}
+		BigInteger solution = p.divide(d);
+		System.out.println(solution + " " + d);
 	}
 	
 	public static boolean addition(BigInteger x1, BigInteger y1, BigInteger x2, BigInteger y2, BigInteger A) {
-		BigInteger lamda;
-		if(x1.equals(x2) && y1.equals(y2))
-			lamda = (((x1.pow(2).multiply(BigInteger.valueOf(3))).add(A)).multiply((y1.multiply(BigInteger.valueOf(2))).modInverse(p))).mod(p);
-		
-		else {
-			try {
-				lamda = ((y2.subtract(y1)).multiply((x2.subtract(x1)).modInverse(p))).mod(p);
-			} catch
+		BigInteger num;
+		BigInteger denom;
+		if(x2.equals(BigInteger.ZERO) && y2.equals(BigInteger.ONE)) {
+			addX = x1;
+			addY = y1;
+			return true;
 		}
-		addX = (lamda.pow(2)).mod(p);
-		addX = (addX.subtract(x1)).mod(p);
+		if(x1.equals(x2)) {
+			if((y1.add(y2).mod(p)).equals(BigInteger.ZERO))
+				return false;
+			num = (x1.pow(2).multiply(BigInteger.valueOf(3)).add(A)).mod(p);
+			denom = (y1.multiply(BigInteger.valueOf(2))).mod(p);
+		}
+		else{
+			num = (y2.subtract(y1)).mod(p);
+			denom = (x2.subtract(x1)).mod(p);
+		}
+		try {
+			denom = denom.modInverse(p);
+		}catch(ArithmeticException e) {
+			d = denom;
+			return false;
+		}
+		BigInteger lamda = num.multiply(denom);
+		addX = lamda.pow(2);
+		addX = addX.subtract(x1);
 		addX = (addX.subtract(x2)).mod(p);
-		addX = addX.mod(p);
 		addY = x1.subtract(addX);
 		addY = addY.multiply(lamda);
-		addY = addY.subtract(y1);
-		addY = addY.mod(p);
+		addY = (addY.subtract(y1)).mod(p);
 		return true;
 	}
 	
 	
-	public static void multiplication(BigInteger x, BigInteger y, BigInteger j, BigInteger A) {
+	public static boolean multiplication(BigInteger x, BigInteger y, BigInteger n, BigInteger A) {
 		BigInteger rx = BigInteger.ZERO;
-		BigInteger ry = BigInteger.ZERO;
-		BigInteger oj = j;
-		BigInteger qx = x;
-		BigInteger qy = y;
-		if(n.equals(BigInteger.ONE))
-		{
-			multX = x;
-			multY = y;
-			return;
-		}
+		BigInteger ry = BigInteger.ONE;
 		while(n.compareTo(BigInteger.ZERO) == 1) {
-			if((j.mod(BigInteger.valueOf(2))).equals(BigInteger.ONE)) {
-				if(j.equals(oj)) {
-					rx = x;
-					ry = y;
-				}
-				else {
-					addition(rx, ry, x, y, A);
-					rx = addX;
-					ry = addY;
-				}
+			if((n.mod(BigInteger.valueOf(2))).equals(BigInteger.ONE)) {
+				addition(x, y, rx, ry, A);
 			}
-			
-			addition(x, y, x, y, A);
-			x = addX;
-			y = addY;
-			
-			j = j.divide(BigInteger.valueOf(2));
+			n = n.divide(BigInteger.valueOf(2));
+			if(!addition(x, y, x, y, A))
+				return false;
 		}
-		multX = rx;
-		multY = ry;
+		multX = addX;
+		multY = addY;
+		return true;
 	}
 }
